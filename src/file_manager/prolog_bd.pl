9eb7249fd48c689999ecb89a3c46a01c34606193
Dayan Bravo <dayan3847@gmail.com>
2022-12-09 00:43:34 -0600

    feat: predecessor

--- src/file_manager/prolog_bd.pl
+++ src/file_manager/prolog_bd.pl
@@ -1,31 +1,46 @@
 
-tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil)))).
+% Test Tree
+% tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil)))).
 
-%% exists in tree
-%% insert(Node, Tree)
-exists(Node, tree(Node, _, _)) :- !.
-exists(Node, tree(_, Left, _)) :- exists(Node, Left), !.
-exists(Node, tree(_, _, Right)) :- exists(Node, Right), !.
-% exists(6,tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil))))).
-% exists(9,tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil))))).
-% exists(2,tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil))))).
+% Find (Ordered Search)
+% find(Node, Tree)
+find(Node, tree(Node, _, _)) :- !.
+find(Node, tree(Parent, Left, _)) :- Node < Parent, find(Node, Left), !.
+find(Node, tree(Parent, _, Right)) :- Node > Parent, find(Node, Right), !.
+% find(6,tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil))))).
 
-% is parent
+% Parent (Direct Parent)
 % parent(Parent, Child, Tree)
-parent(Parent, Child, tree(Parent, Left, _)) :- exists(Child, Left), !.
-parent(Parent, Child, tree(Parent, _, Right)) :- exists(Child, Right), !.
-parent(Parent, Child, tree(_, Left, _)) :- parent(Parent, Child, Left), !.
-parent(Parent, Child, tree(_, _, Right)) :- parent(Parent, Child, Right), !.
-% parent(3,5,tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil))))).
+parent(Parent, Child, tree(Parent, tree(Child, _, _), _)) :- !.
+parent(Parent, Child, tree(Parent, _, tree(Child, _, _))) :- !.
+parent(Parent, Child, tree(Root, Left, _)) :- Parent < Root, parent(Parent, Child, Left), !.
+parent(Parent, Child, tree(Root, _, Right)) :- Parent > Root, parent(Parent, Child, Right), !.
 
-% is a leaf node
+% Predecessor (Is Predecessor)
+% predecessor(Predecessor, Child, Tree)
+predecessor(Predecessor, Child, tree(Predecessor, Left, _)) :- find(Child, Left), !.
+predecessor(Predecessor, Child, tree(Predecessor, _, Right)) :- find(Child, Right), !.
+predecessor(Predecessor, Child, tree(_, Left, _)) :- predecessor(Predecessor, Child, Left), !.
+predecessor(Predecessor, Child, tree(_, _, Right)) :- predecessor(Predecessor, Child, Right), !.
+% predecessor(3,5,tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil))))).
+
+% adyacent(Node, Adyacents, Tree)
+adyacent(Node, append(LeftNode, RightNode, ParentNode), tree(Node, Left, Right)) :- adyacent(Left, AdyacentsLeft), adyacent(Right, AdyacentsRight), append(AdyacentsLeft, AdyacentsRight, Adyacents), !.
+
+% Leaf (Is a leaf node)
 % leaf(Leaf, Tree)
 leaf(Leaf, tree(Leaf, nil, nil)) :- !.
 leaf(Leaf, tree(_, Left, _)) :- leaf(Leaf, Left), !.
 leaf(Leaf, tree(_, _, Right)) :- leaf(Leaf, Right), !.
 % leaf(2,tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil))))).
 
-% insert a node
+% Level (Level of a Tree)
+% level(Level, Tree)
+level(0, nil) :- !.
+level(max(LeftLevel, RightLevel) + 1, tree(_, Left, Right)) :- level(LeftLevel, Left), level(RightLevel, Right), !.
+% level(Level,tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil))))).
+
+% Insert (Insert a node)
 % insert(Node, Tree, NewTree)
 insert(Node, tree(Parent, nil, Right ), tree(Parent, tree(Node, nil, nil), Right)) :- Node < Parent, !.
 insert(Node, tree(Parent, Left, nil ), tree(Parent, Left, tree(Node, nil, nil))) :- Node > Parent, !.
@@ -34,105 +49,41 @@ insert(Node, tree(Parent, Left, Right), tree(Parent, Left, NewRight)) :- Node > 
 % insert(99,tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil)))), NewTree).
 % insert(4.5,tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil)))), NewTree).
 
-% preorder
+% Delete a node
+% delete(Node, Tree, NewTree)
+delete(Node, tree(Node, nil, nil), nil) :- !.
+delete(Node, tree(Node, Left, nil), Left) :- !.
+delete(Node, tree(Node, nil, Right), Right) :- !.
+delete(Node, tree(Node, Left, Right), tree(Parent, NewLeft, Right)) :- find(Parent, Left), delete(Parent, Left, NewLeft), !.
+delete(Node, tree(Node, Left, Right), tree(Parent, Left, NewRight)) :- find(Parent, Right), delete(Parent, Right, NewRight), !.
+delete(Node, tree(Parent, Left, Right), tree(Parent, NewLeft, Right)) :- Node < Parent, delete(Node, Left, NewLeft), !.
+delete(Node, tree(Parent, Left, Right), tree(Parent, Left, NewRight)) :- Node > Parent, delete(Node, Right, NewRight), !.
+% delete(9,tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil)))), NewTree).
+% delete(6,tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil)))), NewTree).
+
+
+% Preorder
 % preorder(Tree, List)
 preorder(nil, []) :- !.
 preorder(tree(Node, Left, Right), [Node|List]) :- preorder(Left, LeftList), preorder(Right, RightList), append(LeftList, RightList, List), !.
-% preorder(tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil)))), List).
+% preorder(tree(6,nil,nil),List).
+% preorder(tree(6,tree(5,nil,nil),nil),List).
+% preorder(tree(6,tree(3,tree(1,nil,nil),tree(4,nil,nil)),nil),List).
+% preorder(tree(6,tree(3,tree(1,nil,nil),tree(4,nil,nil)),tree(7,nil,tree(9,nil,nil))),List).
+% preorder(tree(6,tree(3,tree(1,nil,nil),tree(4,nil,nil)),tree(7,nil,tree(9,nil,tree(10,nil,nil)))),List).
+% preorder(tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,nil)),tree(7,nil,tree(9,nil,tree(10,nil,nil)))),List).
+% preorder(tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,nil,tree(10,nil,nil)))),List).
+% preorder(tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil)))),List).
+% preorder(tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil)))),List).
 
+% Inorder
+% inorder(Tree, List)
+inorder(nil, []) :- !.
+inorder(tree(Node, Left, Right), List) :- inorder(Left, LeftList), inorder(Right, RightList), append(LeftList, [Node|RightList], List), !.
+% inorder(tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil)))),List).
 
-
-%preorder(nil, []).
-%preorder(tree(Root, Left, Right), [Root|List]) :-
-%    preorder(Left, LeftList),
-%    preorder(Right, RightList),
-%    append(LeftList, RightList, List).
-%
-%% inorder
-%inorder(nil, []).
-%inorder(tree(Root, Left, Right), List) :-
-%    inorder(Left, LeftList),
-%    inorder(Right, RightList),
-%    append(LeftList, [Root|RightList], List).
-%
-%% insert
-%insert(X, nil, tree(X, nil, nil)).
-%insert(X, tree(Root, Left, Right), tree(Root, NewLeft, Right)) :-
-%    X =< Root,
-%    insert(X, Left, NewLeft).
-%insert(X, tree(Root, Left, Right), tree(Root, Left, NewRight)) :-
-%    X > Root,
-%    insert(X, Right, NewRight).
-
-
-
-%find(E, t(E, _, _)).
-%find(E, t(Root, L, _)):- E < Root, find(E, L).
-%find(E, t(Root, _, R)):- E > Root, find(E, R).
-%
-%create([], nil).
-%create(List, t(M, L, R)):-
-%    length(List, List_Length),
-%    Mid is List_Length div 2,
-%    length(List_1, Mid),
-%    append(List_1, [M|List_2], List),
-%    create(List_1, L),
-%    create(List_2, R).
-%
-%broad_tour(t(nil, nil, nil), nil).
-%broad_tour(t(E, nil, nil), E).
-%broad_tour(t(E, L, R), [E|Children]):-
-%    append(L, R, Concat),
-%    broad_tour(Concat, Children).
-%
-%
-%tree1(t(2, t(1, nil, nil), t(3, nil, nil))).
-%
-%/*tree(t(6, t(3, t(1, nil, t(2, nil, nil)), t(4, nil, t(5, nil, nil))), t(7, nil, t(9, t(8, nil, nil), t(10, nil, nil))))).*/
-%
-%/*exists_vertex(V, t(V, _, _)).
-%exists_vertex(V, t(Root, L, _)):- V <= Root, exists_vertex(V, L).
-%exists_vertex(V, t(Root, _, R)):- V > Root, exists_vertex(V, L).*/
-%
-%/*add_new_vertex(X, nil, tree(X, nil, nil)).
-%add_new_vertex(X, tree(X, T1, T2), tree(X, T1, T2)).
-%add_new_vertex(X, tree(Y, T1, T2), tree(Y, T1N, T2)):- X<Y, add_new_vertex(X, T1, T1N).
-%add_new_vertex(X, tree(Y, T1, T2), tree(Y, T1, T2N)):- X>Y, add_new_vertex(X, T1, T2N).*/
-%
-%tree([1,[2,[4,[7,nil,nil],nil],[5,nil,nil]],[3,[6,[8,nil,nil],[9,nil, nil]],nil]]).
-%
-%preorder(nil).
-%preorder([Node, FG, FD]) :-
-%	format('~w ', [Node]),
-%	preorder(FG),
-%	preorder(FD).
-%
-%inorder(nil).
-%inorder([Node, FG, FD]) :-
-%	inorder(FG),
-%	format('~w ', [Node]),
-%	inorder(FD).
-%
-%postorder(nil).
-%postorder([Node, FG, FD]) :-
-%	postorder(FG),
-%	postorder(FD),
-%	format('~w ', [Node]).
-%
-%level_order([]).
-%
-%level_order(A) :-
-%	level_order_(A, U-U, S),
-%	level_order(S).
-%
-%level_order_([], S-[],S).
-%
-%level_order_([[Node, FG, FD] | T], CS, FS) :-
-%	format('~w ', [Node]),
-%	append_dl(CS, [FG, FD|U]-U, CS1),
-%	level_order_(T, CS1, FS).
-%
-%level_order_([nil | T], CS, FS) :-
-%	level_order_(T, CS, FS).
-%
-%append_dl(X-Y, Y-Z, X-Z).
+% Postorder
+% postorder(Tree, List)
+postorder(nil, []) :- !.
+postorder(tree(Node, Left, Right), List) :- postorder(Left, LeftList), postorder(Right, RightList), append(LeftList, RightList, TempList), append(TempList, [Node], List), !.
+% postorder(tree(6,tree(3,tree(1,nil,tree(2,nil,nil)),tree(4,nil,tree(5,nil,nil))),tree(7,nil,tree(9,tree(8,nil,nil),tree(10,nil,nil)))),List).
