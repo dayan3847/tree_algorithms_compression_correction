e00f32d83b67452bceaa96f60ae1021b1dd03e7e
Dayan Bravo <dayan3847@gmail.com>
2022-12-09 12:42:20 -0600

    refactor: remove prolog project files

--- python/test/test_integration_prolog.py
+++
@@ -1,9 +0,0 @@
-from pyswip import Prolog
-
-if __name__ == '__main__':
-    prolog = Prolog()
-    prolog.assertz("father(michael,john)")
-
-    # prolog.consult('basic_test.pl')
-    # c = list(prolog.query("test(X,Y)"))
-    # print(c)
