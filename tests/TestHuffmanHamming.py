f4e2ada8306f01ba2c6bafdae7df1b35a748f425
Dayan Bravo <dayan3847@gmail.com>
2022-12-14 09:57:19 -0600

    test: added test calculate frequencies

--- tests/TestHuffmanHamming.py
+++ tests/TestHuffmanHamming.py
@@ -29,8 +29,9 @@ class TestHuffmanHamming(unittest.TestCase):
         self.file_json_codes = f'{TestHuffmanHamming.__path_json__}codes.json'
         self.file_json_tree = f'{TestHuffmanHamming.__path_json__}tree.json'
         self.file_list_full_path: List[str] = []
-        self.verbose = False
-        self.file_list = ['text_hello_world']
+        self.verbose = True
+        self.file_list = []
+        self.file = 'text_hello_world'
 
     def get_file_list_full_path(self) -> List[str]:
         if len(self.file_list_full_path) == 0:
@@ -40,66 +41,84 @@ class TestHuffmanHamming(unittest.TestCase):
 
         return self.file_list_full_path
 
-    def test_encode(self):
+    def test_calculate_frequencies(self):
         if self.verbose:
-            print('test_encode')
+            print('\ntest_calculate_frequencies')
+
+        if len(self.file_list) == 0:
+            self.file_list = [self.file]
 
         frequency_calculator = FrequencyCalculator(self.get_file_list_full_path())
         json_frequency = frequency_calculator.export_json_frequency()
+
+        # Create frequency.json file
         FileManager.write_txt(self.file_json_frequency, json_frequency)
         frequencies = frequency_calculator.frequency_symbols()
 
         huffman = Huffman(frequencies)
 
+        # Create codes
         json_codes = huffman.export_json_codes()
         FileManager.write_txt(self.file_json_codes, json_codes)
 
+        # Create tree
         json_tree = huffman.export_json_tree()
         FileManager.write_txt(self.file_json_tree, json_tree)
 
+    def test_encode(self):
+        if self.verbose:
+            print('\ntest_encode')
+
+        frequency_calculator = FrequencyCalculator()
+        frequency_json: str = FileManager.read_txt(self.file_json_frequency)
+        frequencies: dict[str, float] = frequency_calculator.import_json_frequency(frequency_json)
+
+        huffman = Huffman(frequencies)
+
         hamming = Hamming(self.verbose)
 
-        for file in self.file_list:
-            text_path = f'{TestHuffmanHamming.__path_origin__}{file}.txt'
-            text: str = FileManager.read_txt(text_path)
+        text_path = f'{TestHuffmanHamming.__path_origin__}{self.file}.txt'
+        text: str = FileManager.read_txt(text_path)
 
-            print()
-            ts1 = time.time()
-            if self.verbose:
-                print(f'Begin: encode file "{file}"')
+        print()
+        ts1 = time.time()
+        if self.verbose:
+            print(f'Begin: encode file "{self.file}"')
 
-            encode_huffman: Code = huffman.encode(text)
+        encode_huffman: Code = huffman.encode(text)
 
-            if self.verbose:
-                print(f'End: encode "{file}"')
-            ts2 = time.time()
-            if self.verbose:
-                print(f'Time: {ts2 - ts1}\n')
+        if self.verbose:
+            print(f'End: encode "{self.file}"')
+        ts2 = time.time()
+        if self.verbose:
+            print(f'Time: {ts2 - ts1}\n')
 
-            FileManager.write_bin(f'{TestHuffmanHamming.__path_bin__}{file}.huffman.bin', encode_huffman)
+        FileManager.write_bin(f'{TestHuffmanHamming.__path_bin__}{self.file}.huffman.bin', encode_huffman)
 
-            if self.verbose:
-                print(f'File: {file}')
-                print('Text Read:')
-                print('>>>>>>>>>>')
-                print(text)
-                print('<<<<<<<<<<')
-                print(f'Code Write: {encode_huffman}\n')
+        if self.verbose:
+            print(f'File: {self.file}')
+            print('Text Read:')
+            print('>>>>>>>>>>')
+            print(text)
+            print('<<<<<<<<<<')
+            print(f'Code Write: {encode_huffman}\n')
 
-            encode_hamming: Code = hamming.encode(encode_huffman)
+        encode_hamming: Code = hamming.encode(encode_huffman)
 
-            FileManager.write_bin(f'{TestHuffmanHamming.__path_bin__}{file}.hamming.bin', encode_hamming)
+        FileManager.write_bin(f'{TestHuffmanHamming.__path_bin__}{self.file}.hamming.bin', encode_hamming)
 
-            FileManager.write_bin(f'{TestHuffmanHamming.__path_bin__}Entrada.bin', encode_hamming)
+        FileManager.write_bin(f'{TestHuffmanHamming.__path_bin__}Entrada.bin', encode_hamming)
 
     def test_generate_errors(self):
+        if self.verbose:
+            print('\ntest_generate_errors')
         # run bash script
         bash_script = 'bash ../txt/bin/script_generate_errors.sh'
         os.system(bash_script)
 
     def test_decode(self):
         if self.verbose:
-            print('test_decode')
+            print('\ntest_decode')
 
         frequency_calculator = FrequencyCalculator()
         frequency_json: str = FileManager.read_txt(self.file_json_frequency)
@@ -108,34 +127,37 @@ class TestHuffmanHamming(unittest.TestCase):
         huffman = Huffman(frequencies)
         hamming = Hamming(self.verbose)
 
-        for file in self.file_list:
-            encode: Code = FileManager.read_bin(f'{TestHuffmanHamming.__path_bin__}Salida.bin')
+        encode: Code = FileManager.read_bin(f'{TestHuffmanHamming.__path_bin__}Salida.bin')
 
-            hamming_decode: Code = hamming.decode(encode)
+        hamming_decode: Code = hamming.decode(encode)
 
-            if hamming.report:
-                FileManager.write_txt(f'{TestHuffmanHamming.__path_bin__}ReporteCorrecciones.txt', hamming.report_text)
-            FileManager.write_bin(f'{TestHuffmanHamming.__path_bin__}{file}.corrected.bin', hamming_decode)
+        if hamming.report:
+            FileManager.write_txt(f'{TestHuffmanHamming.__path_bin__}ReporteCorrecciones.txt', hamming.report_text)
+        FileManager.write_bin(f'{TestHuffmanHamming.__path_bin__}{self.file}.corrected.bin', hamming_decode)
 
-            print()
-            ts1 = time.time()
-            print(f'Begin: decode file "{file}"')
-            decode: str = huffman.decode(hamming_decode)
-            print(f'End: decode "{file}"')
-            ts2 = time.time()
+        print()
+        ts1 = time.time()
+        if self.verbose:
+            print(f'Begin: decode file "{self.file}"')
+        decode: str = huffman.decode(hamming_decode)
+        if self.verbose:
+            print(f'End: decode "{self.file}"')
+        ts2 = time.time()
+        if self.verbose:
             print(f'Time: {ts2 - ts1}\n')
 
-            FileManager.write_txt(f'{TestHuffmanHamming.__path_decode__}{file}.txt', decode)
-            if self.verbose:
-                print(f'File: {file}')
-                print(f'Code Read: {encode}')
-                print('Text Write (Decoded):')
-                print('>>>>>>>>>>')
-                print(decode)
-                print('<<<<<<<<<<')
-                print()
+        FileManager.write_txt(f'{TestHuffmanHamming.__path_decode__}{self.file}.txt', decode)
+        if self.verbose:
+            print(f'File: {self.file}')
+            print(f'Code Read: {encode}')
+            print('Text Write (Decoded):')
+            print('>>>>>>>>>>')
+            print(decode)
+            print('<<<<<<<<<<')
+            print()
 
     def test_complete(self):
+        self.test_calculate_frequencies()
         self.test_encode()
         self.test_generate_errors()
         self.test_decode()
--- tests/TestHuffmanHammingDev.py
+++ tests/TestHuffmanHammingDev.py
@@ -7,19 +7,20 @@ class TestHuffmanHammingDev(TestHuffmanHamming):
 
     def setUp(self):
         super().setUp()
-        # self.verbose = True
-        self.file_list = [
-            # 'text_alejo_carpentier_los_pasos_perdidos',
-            # 'text_carlos_fuentes_aura',
-            # 'text_fernando_del_paso_palinuro_de_mexico',
-            # 'text_gabriel_garcia_marquez_cien_años_de_soledad',
-            # 'text_jorge_ibarguengoitia_estas_ruinas_que_ves',
-            # 'text_juan_rulfo_el_llano_en_llamas_cuento',
-            # 'text_juan_rulfo_pedro_paramo',
-            # 'text_julio_cortazar_rayuela',
-            # 'text_mario_vargas_llosa_pantaleon_y_las_visitadoras',
-            'text_hello_world',
-        ]
+        self.verbose = False
+        # self.file_list = [
+        #     'text_alejo_carpentier_los_pasos_perdidos',
+        #     'text_carlos_fuentes_aura',
+        #     'text_fernando_del_paso_palinuro_de_mexico',
+        #     'text_gabriel_garcia_marquez_cien_años_de_soledad',
+        #     'text_jorge_ibarguengoitia_estas_ruinas_que_ves',
+        #     'text_juan_rulfo_el_llano_en_llamas_cuento',
+        #     'text_juan_rulfo_pedro_paramo',
+        #     'text_julio_cortazar_rayuela',
+        #     'text_mario_vargas_llosa_pantaleon_y_las_visitadoras',
+        #     'text_hello_world',
+        # ]
+        self.file = 'text_alejo_carpentier_los_pasos_perdidos'
 
     def test_encode(self):
         super().test_encode()
